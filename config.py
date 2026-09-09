ee905dae6a525489c493c17567937e98050d76dc
after620cafe-collab <after6.20cafe@gmail.com>
2026-09-09 22:03:15 +0530

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-PREFIX = os.getenv("PREFIX", "-")
-AI_MODEL = os.getenv("GROQ_MODEL", "openai/gpt-oss-20b")
-MAX_MEMORY_MESSAGES = int(os.getenv("MAX_MEMORY_MESSAGES", "12"))
-MAX_REPLY_CHARS = 1900
